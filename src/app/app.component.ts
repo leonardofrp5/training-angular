9cbba156c05fde012939bf885ed99bc7b7ca6d9c
leonardofrp5 <leonardofrp5@gmail.com>
2019-11-27 12:42:09 -0500

    solution comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,29 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  rForm: FormGroup;
-  post: any;
-  firstName: string = '';
-  lastName: string = '';
-  email: string = '';
-  passwordConfirmation: string = '';
-  messageAlert: string = 'Campo obligatorio'
 
-  constructor(private fb: FormBuilder){
-    this.rForm = fb.group({
-      'firstName': [null, Validators.compose([Validators.required, Validators.minLength(3)])],
-      'lastName': [null, Validators.compose([Validators.required, Validators.minLength(3)])],
-      'email': [null, Validators.compose([Validators.required, Validators.email])],
-      'passwordConfirmation': [null, Validators.compose([Validators.required, Validators.maxLength(20), Validators.minLength(5)])]
-    });
-  }
-
-  addPost(post) {
-    this.firstName = post.firstName;
-    this.lastName = post.lastName;
-    this.email = post.email;
-    this.passwordConfirmation = post.passwordConfirmation;
-    console.log(post);
-  }
+  constructor(){}
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormRegisterComponent } from './form-register/form-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormRegisterComponent
   ],
   imports: [
     BrowserModule,
